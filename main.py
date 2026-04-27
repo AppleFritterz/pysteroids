34040ac5e4ba8112cc9e15924f42995efde8eea3
Fritz <mfrita70@gmail.com>
2026-04-26 22:27:15 -0500

    added fps lock/game clock

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -17,6 +20,7 @@ def main():
             pass
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
