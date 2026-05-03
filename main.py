2d266acb745cca5905152cee8eb2b232d70522ac
Fritz <mfrita70@gmail.com>
2026-05-02 23:46:47 -0500

    fixed .gitignore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from logger import log_state
+from logger import log_state, log_event
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from player import Player
 from asteroid import Asteroid
